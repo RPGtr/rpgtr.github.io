ac18123eb4a8bc94e66d25525390b88b5411869e
RPGtr <rossoline@yahoo.com>
2024-08-04 20:53:41 -0400

    Delete scripts/main.js

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-
-var firstlink = document.getElementsByTagName('video1')[0];
-firstlink.addEventListener("click", function (event) {
-    event.preventDefault();
-    video1.currentTime = 7;
-    video1.play();
-}, false);
